00202e29d012c500fa434ea2e081ad87615996da
Gabriel Augusto Morato dos Santos <30707042+GabrielAugusto1996@users.noreply.github.com>
2023-07-04 15:05:00 +0200

    Add files via upload

---
+++ backend/src/main/java/com/learning/spring/reactive/LearningSpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringReactiveApplication.class, args);
+	}
+
+}
